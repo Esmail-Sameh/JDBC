b401b2c39316d939d0517656232aeef47e138e2c
Esmail Sameh <esmil.sameh2005@gmail.com>
2025-02-07 16:49:51 +0200

    Create Statement with out loop

--- src/DemoJdbc.java
+++ src/DemoJdbc.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.Collection;
 
 public class DemoJdbc {
 
@@ -14,11 +15,19 @@ public class DemoJdbc {
 
     public static void main(String[] args) throws Exception {
         String url = "jdbc:postgresql://localhost:8080/demo";
-        String uname= "postgres";
-        String pass= "0000";
-
-        Connection con = DriverManager.getConnection(url,uname,pass);
+        String uname = "postgres";
+        String pass = "0000";
+        String sql = "select * from students";
+        Connection con = DriverManager.getConnection(url, uname, pass);
         System.out.println("Connection established");
+        Statement stmt = con.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        rs.next();
+        System.out.println("your name is: " + rs.getString("sname"));
+        con.close();
+        System.out.println("Collection is closed");
+
+
     }
 
 }
